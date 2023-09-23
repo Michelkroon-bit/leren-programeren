575f52e3d286ee8d9f12935b9148d9e3786ee5ab
Michelkroon-bit <99076850@mydavinci.nl>
2023-09-23 11:14:46 +0200

    freelunch

--- module 1/feestlunch.py
+++ module 1/feestlunch.py
@@ -1,10 +1,7 @@
-type=croissantjes = 0.39 * 17
-type= stokbroden = 2.78 * 2
-type= kortingsbonnen= 0.50* 3
+croissantjes = 0.39 * 17
+stokbroden = 2.78 * 2
+kortingsbonnen= 0.50* 3
 
-print(croissantjes = 0.39 * 17)
-print(stokbroden = 2.78 * 2)
-print(kortingsbonnen= 0.50* 3)
-
-# uitkomst = croissantjes + stokbroden - kortingsbonnen
-# print (uitkomst)
+uitkomst = croissantjes + stokbroden - kortingsbonnen
+print(uitkomst)
+print(type(croissantjes))
