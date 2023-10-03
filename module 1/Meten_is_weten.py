4e0464b7c8b1cde844bb2f59214778f7309b4e3d
Michelkroon-bit <99076850@mydavinci.nl>
2023-10-03 11:48:48 +0200

    “input en if-statement”

---
+++ module 1/Meten_is_weten.py
@@ -0,0 +1,7 @@
+a= int(input('vul een getal in'))
+b= int(input('vul een getal in'))
+
+if a > b:
+    max=a
+    min=b
+    
--- module 1/speelhal.py
+++ module 1/speelhal.py
@@ -3,23 +3,25 @@ vip_vr_gameset=int(input('hoeveel minuten vr wil je '))
 
 
 
-PRIJS_PER_PERSOON=745 #CENT
-VIP_VR_GAMESET=37#CENT
+PRIJS_PER_PERSOON=7.45 #CENT
+VIP_VR_GAMESET=0.37#CENT
 VR_PRIJS_PER_MINUTEN=5
 
 
 
-uitkomst = PRIJS_PER_PERSOON * aantal_personen
+uitkomst = PRIJS_PER_PERSOON * aantal_personen*100
 print (f'de prijs per persoon is',uitkomst,'cent')
 
 
 vr_vip1 = VIP_VR_GAMESET /VR_PRIJS_PER_MINUTEN
-vr_vip2 = vr_vip1 * vip_vr_gameset
+vr_vip2 = vr_vip1 * vip_vr_gameset *100
 
-uitkomst_vip2 = (vr_vip1 * aantal_personen)
+uitkomst_vip2 = (vr_vip1 * aantal_personen)*100
 totaal = int(round(vr_vip1 , 2))
 print ('de prijs voor de vip vr gameseat is',uitkomst_vip2,'cent')
-totaal = uitkomst + uitkomst_vip2
+totaal = uitkomst + uitkomst_vip2/100
 
 
-print(f'Dit geweldige dagje-uit met {aantal_personen} mensen in de Speelhal met 45 minuten VR kost je maar',totaal,'cent')
+print(f'Dit geweldige dagje-uit met {aantal_personen} mensen in de Speelhal met {aantal_personen} personen voor {vip_vr_gameset} minuten VR kost je maar {totaal}')
+
+#todo:printjes omrekenen in euros
