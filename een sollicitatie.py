b6f20e8d05de22b76652364334ff449299fd5f53
Michelkroon-bit <99076850@mydavinci.nl>
2023-10-05 22:42:47 +0200

    sollositatie.py

--- een sollicitatie.py
+++ een sollicitatie.py
@@ -15,6 +15,10 @@ print('als blijkt dat u aan de critica voldoet dan komt u \nin aanmerking voor e
 print('Ontspan maar blijf wakker , hier komen de vragen')
 print('+++++++++++++++++++++++++++++++++++++++++++++++++++++')
 
+
+
+
+
 naam=input('wat is uw naam? ')
 netto_lichaamslengte = int(input('Wat is uw netto lichaamslengte in hele cm, dus exclusief uw kapsel? '))
 lichaamsgewicht=int(input('Wat is uw lichaamsgewicht in hele kg? '))
@@ -27,6 +31,11 @@ opruimerig=int(input('Hoe opruimerig schat u uzelf op schaal van 1 (niet) tot 5 
 vliegbevet=input('Bezit u een vliegbevet? J/N ')
 is_de_aarde_plat=input('Is volgens u de aarde plat? ')
 ruimte_nodig=int(input('Hoeveel ruimte heeft u nodig om u heen op schaal van 1 (niet) tot 5 (veel)? '))
+
+if naam == '': 
+    bool_naam = False
+else:
+    bool_naam = True
 
 if netto_lichaamslengte >= 150 and netto_lichaamslengte <= 220:
     netto_lichaamslengte=True
@@ -53,15 +62,37 @@ if praktijkervaring_met_acrobatiek >=3:
 else:
     praktijkervaring_met_acrobatiek= False
 
-if certificaat == 'ja JA J YES':
+if certificaat in ["j", "ja" , "JA" , "J", "YES"]:
     certificaat = True
 else:
     certificaat = False
     
-if vliegbevet == 'j ja J JA Y YES':
+if vliegbevet in ['j', 'ja', 'J', 'JA' , 'Y',  'YES']:
     vliegbevet=True
 else:
     vliegbevet=False
 
-if naam and netto_lichaamslengte and lichaamsgewicht and certificaat and praktijkervaring_met_acrobatiek and praktijkervaring_met_dieren_dressuur and ervaring_met_jongleren and fysieke_sport and opruimerig and vliegbevet and is_de_aarde_plat and ruimte_nodig == True:
-    print(f'beste{naam} \n Proficiat! u komt in aanmerking voor een sollicitatie gesprek, stuur sel uw CV')
+if fysieke_sport >= 1:
+    fysieke_sport= True
+else:
+    fysieke_sport = False
+
+if opruimerig >= 1 and opruimerig  <=5:
+    opruimerig = True
+else:
+    opruimerig = False
+    
+if is_de_aarde_plat in ["n" , "nee" , "N" , "NEE" , "no" ,"NO"]:
+    is_de_aarde_plat = True
+else:
+    is_de_aarde_plat=False
+    
+if ruimte_nodig >= 1 and ruimte_nodig <= 5:
+    ruimte_nodig=True
+else:
+    ruimte_nodig=False
+
+if bool_naam and netto_lichaamslengte and lichaamsgewicht and certificaat and praktijkervaring_met_acrobatiek and praktijkervaring_met_dieren_dressuur and ervaring_met_jongleren and fysieke_sport and opruimerig and vliegbevet and is_de_aarde_plat and ruimte_nodig :
+    print(f'beste, {naam} \n Proficiat! u komt in aanmerking voor een sollicitatie gesprek, stuur sel uw CV')
+else:
+    print('u bent niet gecwalificeert voor een gesprek :(')
