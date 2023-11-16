e1b6e8f2b6b7f5e5b32aa05f54d38be02cae746f
Michelkroon-bit <99076850@mydavinci.nl>
2023-11-16 20:50:09 +0100

    example 6

--- module 2/forever_young/robotarm-python-2023-main/example #6.py
+++ module 2/forever_young/robotarm-python-2023-main/example #6.py
@@ -4,6 +4,19 @@ robotArm = RobotArm('exercise 6')
 
 # Jouw python instructies zet je vanaf hier:
 
+#wit
+for x in range (3):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
 
+    #rood
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+    robotArm.moveLeft()
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
