2264310daf57662a7a0c24c52ab47cd55b3e8b11
Michelkroon-bit <99076850@mydavinci.nl>
2023-11-28 16:18:45 +0100

    example 7

--- module 2/forever_young/robotarm-python-2023-main/example #4 .py
+++ module 2/forever_young/robotarm-python-2023-main/example #4 .py
@@ -12,19 +12,14 @@ for x in range(5):
     	for z in range (2):
             robotArm.moveLeft()
 
-# # VOOR LAATSTE BLOK
-# robotArm.grab()
-# for x in range (2):
-#     robotArm.moveRight()
-# robotArm.drop()
 
 # WEER TERUG
 for x in range(4):
     robotArm.grab()
-    for x in range(1):
+    for a in range(1):
         robotArm.moveLeft()
     robotArm.drop()
-    for x in range(1):
+    for b in range(1):
         robotArm.moveRight()
 
 # VOOR LAATSTE BLOK
--- module 2/forever_young/robotarm-python-2023-main/example #7.py
+++ module 2/forever_young/robotarm-python-2023-main/example #7.py
@@ -1,14 +1,26 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 7')
-count = 0 
+# blokje = 0 
 # Jouw python instructies zet je vanaf hier:
-while count < 7:
-    robotArm.moveRight
-    count+=1
-
+afstand = 0 
+while afstand < 9:  
+    robotArm.moveRight()
+    afstand +=1   
+stapeltjes = 0
+while stapeltjes < 5:
+    blokje = 0
+    while blokje < 6:
+        
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+        if blokje <5:
+            robotArm.moveRight()
+        blokje+=1  
+    if stapeltjes < 4:
+        robotArm.moveLeft()
+    stapeltjes +=1
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
 
-
-#
