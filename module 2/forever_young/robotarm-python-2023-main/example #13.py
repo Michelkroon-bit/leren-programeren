f94967d437b7614758670636dc15defd647d0e6a
Michelkroon-bit <99076850@mydavinci.nl>
2023-11-30 19:23:28 +0100

    example 13

--- module 2/forever_young/robotarm-python-2023-main/example #13.py
+++ module 2/forever_young/robotarm-python-2023-main/example #13.py
@@ -1,9 +1,23 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 13')
-
+robotArm = RobotArm('exercise 12')
+stappen = 8
+herhalingen = 0
+aantal_stappen = 0
 # Jouw python instructies zet je vanaf hier:
-
-
+while herhalingen <5:
+    while aantal_stappen <stappen:
+        robotArm.moveRight()   
+        aantal_stappen +=1
+    robotArm.grab()
+    aantal_stappen = 0
+    
+  
+    while aantal_stappen <stappen:
+        robotArm.moveLeft()
+        aantal_stappen +=1
+    aantal_stappen=0
+    robotArm.drop()  
+    stappen -= 1
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
