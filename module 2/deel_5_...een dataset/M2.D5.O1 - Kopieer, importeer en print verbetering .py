19b30d1a62ccd65e6e7c4aa1df4165c841d84923
Michelkroon-bit <99076850@mydavinci.nl>
2023-12-15 11:09:46 +0100

    commit

--- module 2/deel_5_...een dataset/M2.D5.O1 - Kopieer, importeer en print verbetering .py
+++ module 2/deel_5_...een dataset/M2.D5.O1 - Kopieer, importeer en print verbetering .py
@@ -1,12 +1,12 @@
 from fruitmand import *
 
-print("rommelige lijst")
+print("rommelige lijst:")
 print(fruitmand)
 print("")
 print("")
 # for extra 
 
-print("zelfde lijst maar dan geordend")
+print("zelfde lijst maar dan geordend:")
 for x in fruitmand:
     print(x)
     
