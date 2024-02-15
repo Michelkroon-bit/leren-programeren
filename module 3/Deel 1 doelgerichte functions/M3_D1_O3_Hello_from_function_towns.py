84e510a2e323baefdae8f7bc17e32d425aeff812
Michelkroon-bit <99076850@mydavinci.nl>
2024-02-15 11:03:09 +0100

    15-02-2024

---
+++ module 3/Deel 1 doelgerichte functions/M3_D1_O3_Hello_from_function_towns.py
@@ -0,0 +1,6 @@
+def welcome_to_function_town(getal):
+    for x in getal:
+        print(f'hello from function town {x}')
+        
+welkom = welcome_to_function_town(5)
+print(welkom)
---
+++ module 3/Deel 1 doelgerichte functions/hello.py
@@ -0,0 +1,4 @@
+from M3_D1_O3_Hello_from_function_towns import welcome_to_function_town
+
+welkom = welcome_to_function_town(7)
+print(welkom)
